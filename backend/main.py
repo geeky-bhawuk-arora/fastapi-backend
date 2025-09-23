2353c4d13d8b31112ba8deabcf994606ac24986f
Bhawuk Arora <bhawukarora042@gmail.com>
2025-09-23 10:17:51 +0530

    updated main

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI (
+    title="Choose your own adventure game API",
+    description="API to generate cool stories",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
-if __name__ == "__main__":
-    main()
+app.add_middleware (
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "_main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
